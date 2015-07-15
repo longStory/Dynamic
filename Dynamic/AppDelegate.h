5a27faece97c2f0b755925fea482182ded052968
zengqi <zengqi@zengqi.local>
2015-07-15 13:50:15 +0800

    Initial Commit

---
+++ Dynamic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dynamic
+//
+//  Created by zengqi on 15/7/15.
+//  Copyright (c) 2015年 zengqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Dynamic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dynamic
+//
+//  Created by zengqi on 15/7/15.
+//  Copyright (c) 2015年 zengqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
